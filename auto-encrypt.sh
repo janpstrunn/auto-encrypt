af32b4391ed40698eab707d1c41cc4f5d41f36f8
Janpstrunn <156332097+janpstrunn@users.noreply.github.com>
2024-01-15 12:38:03 +0000

    Added hashing to the script

--- auto-encrypt.sh
+++ auto-encrypt.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <File to Encrypt>"
+    echo "Usage: $0 <path/to/file or path/to/folder>"
     exit 1
 fi
 file2enc="$1"
 chuck_prefix="chuck"
-encrypted_folder="enc_folder"
+encrypted_folder="Encrypted_Vault"
 if [ ! -d "mount" ]; then
     mkdir "mount"
+fi
+if [ ! -d "Encrypted_Vault" ]; then
+    mkdir "Encrypted_Vault"
 fi
 if [ -d "$encrypted_folder" ]; then
     if [ ! -f "$encrypted_folder/gocryptfs.conf" ]; then
@@ -15,6 +18,7 @@ if [ -d "$encrypted_folder" ]; then
     fi
 fi
 tar -czvf "${chuck_prefix}.tar.gz" "$file2enc"
+sha512sum "${chuck_prefix}.tar.gz" > "hash.sha512"
 split -n l/15 "${chuck_prefix}.tar.gz" "${chuck_prefix}_"
 rm "${chuck_prefix}.tar.gz"
 gocryptfs "$encrypted_folder" mount
